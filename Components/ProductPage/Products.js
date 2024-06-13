3fa69cb213d6375514669a07d20d19c97bd51c42
papapaulosegundo <pcesar.muchalski@gmail.com>
2024-06-13 20:16:39 -0300

    pagina de cadastro de produtos criada

---
+++ Components/ProductPage/Products.js
@@ -0,0 +1,29 @@
+function validarCadastroProduto() {
+    // Seleciona o formulário
+        const formulario = document.getElementById("productsRegistrationForm");
+    // Obtém os dados do formulário
+         const formData = new FormData(formulario);
+          // Envia os dados usando a API Fetch
+          fetch("products.php", {
+             method: "POST",
+             body: formData,
+          })
+             .then((response) => response.json())
+             .then((data) => {
+              document.getElementById("mensagem").innerHTML = data.mensagem;
+                    if( data.mensagem == "ok" ) {
+                          alert("Produto cadastrado, será alocado a página assim que possível!");
+                          window.location.href = "components/menu/menu.html"
+                      } else {
+                        document.getElementById("mensagem").innerHTML = data.mensagem;
+                      }                 
+                })
+                .catch((error) => {
+                  console.error("Erro:", error);
+                  alert("Ocorreu um erro ao enviar o formulário.");
+                });     
+          }
+    
+    function menu() {
+       window.location.href = "components/menu/menu.html";
+    }
