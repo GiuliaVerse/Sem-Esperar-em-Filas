fd4e82d1eece0fec6c924ad28b8153cc3b94ff6e
Paulo César Muchalski <148829920+papapaulosegundo@users.noreply.github.com>
2024-10-24 08:56:13 -0300

    SPRINT 2 - OO1- Busca de Restaurantes  (#35)
    * SEF-OO1- Busca de Restaurantes  funcionando (sem css)
    
    * feat: adds new style and js
    
    * fix: frases
    
    * Atualização no css
    
    Tudo correto na busca de restaurantes
    ---------
    
    Co-authored-by: GiuliaVerse <giulia.casteluci@pucpr.edu.br>

--- Components/RestaurantesBusca/restaurantsPage.php
+++ Components/RestaurantesBusca/restaurantsPage.php
@@ -1,8 +1,9 @@
 <?php
-	require('../../valida_sessao.php');
+    include('Restaurants.php');
+    require('../../valida_sessao.php');
     if ($_SESSION["tipo"] != "cliente") {
-        $url = "Location: /" . $url . "/index.php";             // Monta URL para redirecionamento
-        header($url);                                           // Vai para a página de login / inicial
+        $url = "Location: /" . $url . "/index.php";
+        header($url);
         exit();    
     }
 ?>
@@ -14,6 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Food Delivery - Busca de Restaurantes</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link rel="stylesheet" href="restaurantsPage.css">
 </head>
 
@@ -22,17 +24,55 @@
         <iframe src="../../Components/nav/nav.php" width="100%" height="100"></iframe>
     </header>
 
-    <div class="container">
-        <h2>Restaurantes Disponíveis</h2>
-        <input type="text" id="buscaInput" placeholder="Search for restaurantes..." onkeyup="searchrestaurantes()">
-        <div id="restauranteList"></div>
-    </div>
+<div class="container">
 
-    <script src="Restaurants.js"></script>
+   <!-- Seção de busca de restaurantes -->
+   <section class="search-section">
+      <form>
+          <input name="busca" value="<?php if(isset($_GET['busca'])) echo $_GET['busca']; ?>" placeholder="Digite o restaurante procurado" type="text" >
+          <button type="submit"> Pesquisar Restaurante</button>
+      </form>
+      <br>
+      <table width="600px" border="1">
+          <tr>
+              <th>Restaurante</th>
+          </tr>
+          
+          <?php
+          if (!isset($_GET['busca'])) {
+              ?>
+          <tr>
+              <td colspan="1">Digite o restaurante para pesquisar</td>
+          </tr>
+          <?php
+          } else {
+              $pesquisa = $mysqli ->real_escape_string($_GET['busca']);
+              $sql_code = " SELECT * FROM restaurante WHERE nome_fantasia LIKE '%$pesquisa%' ";
+              $sql_query = $mysqli ->query($sql_code) or die("Erro ao consultar! " . $mysqli ->error);
 
-    <footer>
-        <iframe src="../Footer/footer.html" width="100%" height="100"></iframe>
-    </footer>
+              if($sql_query -> num_rows == 0) {
+          ?>
+          <tr>
+              <td colspan="1">Nenhum restaurante encontrado ...</td>
+          </tr>
+          <?php 
+          } else {
+              while($dados = $sql_query -> fetch_assoc()) {
+                  ?>
+                  <tr>
+                      <td><?php echo $dados['nome_fantasia']; ?> </td>
+                  </tr>
+                  <?php
+              }
+          }
+          ?>
+
+          <?php
+          } ?>
+          
+      </table>
+   </section>
+<script src="Restaurants.js"></script>
+
 </body>
-
 </html>
